14d76a58a23f2af2cf5718d52a14279cb415372b
Victor Samuel Mosquera Artamonov <victorsamuelmd@gmail.com>
2015-04-19 12:50:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ if ( isset ( $_SESSION['grupo'] ) ) {
  header("Location: adentro.php");
 } 
 include_once("librerias/conex.php"); 
-$link=Conectarse(); 
-$usuario=$_SESSION['usuario'];
+$link = Conectarse(); 
+$usuario = $_SESSION['usuario'];
 include_once("includes/datos.php");
   // No almacenar en el cache del navegador esta página.
 		header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");             		// Expira en fecha pasada
@@ -19,7 +19,7 @@ include_once("includes/datos.php");                                 		// HTTP/1.
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml"xml:lang="es" lang="es" dir="ltr">
 <head>  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<title>Administracion de Usuarios <? echo $empresa; ?></title>
+<title>Administracion de Usuarios <?php echo $empresa; ?></title>
 
  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
  <link rel="SHORTCUT ICON" href="icono.ico">
@@ -30,23 +30,25 @@ include_once("includes/datos.php");                                 		// HTTP/1.
 </head>
 <body >
 
-<?  echo $_SESSION['usuario_login'] ; ?>
+<?php  echo $_SESSION['usuario_login'] ; ?>
 <form action="includes/control.php" method="POST">
 <table width="80%" border="0" align="center">  
 <tr>
    <td colspan="3" align="center">
    
-   <br><br><img src="images/logo.gif"  border="0" title="<? echo $empresa; ?>" alt="<? echo $empresa; ?>" align="top"><br>
+   <br><br><img src="images/logo.gif"  border="0" title="<?php echo $empresa; ?>" alt="<? echo $empresa; ?>" align="top"><br>
   </td>  
 </tr>
 <tr>
     <td colspan="2" align="center" 
-	<?if ($_REQUEST["errorusuario"]=="si"){?>
-		bgcolor=red><span style="color:ffffff"><b>Datos incorrectos</b></span>
-	<?php }elseif($_REQUEST["errorusuario"]=="inactivo"){echo "bgcolor=red><span style='color:ffffff'><b>Usuario inactivo</b></span>"; }
-	else{?>
-		bgcolor=#cccccc>Por favor ingrese los siguientes datos:
-	<?}?></td>
+	<?php if($_REQUEST["errorusuario"]=="si") {
+		echo "bgcolor=red><span style="color:ffffff"><b>Datos incorrectos</b></span>";
+	} elseif($_REQUEST["errorusuario"]=="inactivo") {
+	    echo "bgcolor=red><span style='color:ffffff'><b>Usuario inactivo</b></span>";
+	} else {
+		echo "bgcolor=#cccccc>Por favor ingrese los siguientes datos:";
+	}?>
+	</td>
 </tr>
 
 <tr>
@@ -70,46 +72,3 @@ include_once("includes/datos.php");                                 		// HTTP/1.
 
 </form>
 <br>
-
-<?php
-$control_version = '0aa0b6b3207f0b3839381db1962574a2'; 
-/*  
-
-    IMPORTANTE: Esta aplicación fue desarrollada en el marco del proyecto - Opus libertati- 
-    plan piloto de investigación aplicada en tecnologías de la información y la comunicación 
-    para el sector salud - linea software libre - http://opuslibertati.org
-    promovido por el Hospital Departamental Universitario del Quindio San Juan De Dios.
-    Si deseas participar, apoyar o colaborar con el desarrollo de esta aplicación o conocer otras   
-    lineas de trabajo de nuestro proyecto comunicate con http://ourproject.org/projects/opuslibertati/ 
-    o http://opuslibertati.org
-
-    ATENCION: Puede existir una versión mas reciente de este archivo en 
-    http://ourproject.org/projects/opuslibertati/
-    por favor compruébelo antes de modificarlo. versión actual: [$version]
-    
-    Copyright ©  13-22-2/ 17-Dic-2008 Dirección nacional de derechos de autor Colombia 
-    El core y base de datos inicial de la aplicación fue desarrollado por http://GaleNUx.com 
-    Es un sistema para de información para la salud adaptado al sistema de salud Colombiano.
-    
-    Si necesita consultoría o capacitación en el manejo, instalación y/o soporte o 
-    ampliación de prestaciones de GaleNUx por favor comuníquese al email praxis@galenux.com.
-
-    Este programa es software libre: usted puede redistribuirlo y/o modificarlo 
-    bajo los términos de la Licencia Publica General GNU publicada 
-    por la Fundación para el Software Libre, ya sea la version 3 
-    de la Licencia, o cualquier version posterior.
-
-    Este programa se distribuye con la esperanza de que sea útil, pero 
-    SIN GARANTIA ALGUNA; ni siquiera la garantía implícita 
-    MERCANTIL o de APTITUD PARA UN PROPOSITO DETERMINADO. 
-    Consulte los detalles de la Licencia Publica General GNU para obtener 
-    una información mas detallada. 
-
-    Debería haber recibido una copia de la Licencia Publica General GNU 
-    junto a este programa. 
-    En caso contrario, consulte <http://www.gnu.org/licenses/>.
-    
-    POR FAVOR CONSERVE ESTA NOTA SI EDITA ESTE ARCHIVO 
-
- */
-?>
